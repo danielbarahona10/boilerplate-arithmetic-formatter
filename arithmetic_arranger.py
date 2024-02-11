107f6353e035b7c917ef4c6ca9bcd3bf49626015
danielbarahona10 <79060991+danielbarahona10@users.noreply.github.com>
2024-02-11 04:56:48 +0000

    All the operations and validations
    All the operations are in different lines and validations.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -4,12 +4,27 @@ def arithmetic_arranger(problems):
   
   if len(problems) > 5:
     return "Error: Too many problems."
+
+  for problem in problems:
+    problem = problem.split()
+    if problem[1] != '+' and problem[1] != '-':
+      return "Error: Operator must be '+' or '-'."
+    if len(problem[0]) > 4 or len(problem[2]) > 4:
+      return "Error: Numbers cannot be more than four digits."
+    try:
+      int(problem[0])
+      int(problem[2])
+    except Exception:
+      return "Error: Numbers must only contain digits."
+    
+    length = max(len(problem[0]), len(problem[2]))+2
+    spaces_first_line = length - len(problem[0])
+    spaces_second_line = length - len(problem[2])-1
   
-  a = problems[0].split(' + ')
-  length = max(len(a[0]), len(a[1]))+2
-  spaces_first_line = length - len(a[0])
-  spaces_second_line = length - len(a[1])-1
-  
-  print(' '*spaces_first_line+a[0]+'\n+'+' '*spaces_second_line+a[1]+'\n'+'-'*length)
+    first_line = ' '*spaces_first_line+problem[0]
+    second_line = ' '*spaces_second_line+problem[2]
+    third_line = '-'*length
+    
+    print(first_line+'\n+'+second_line+'\n'+third_line+'\n')
   
   return arranged_problems
