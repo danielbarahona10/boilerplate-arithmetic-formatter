5bb5ea456b8dd71202c33b3620fe1de034d72605
danielbarahona10 <79060991+danielbarahona10@users.noreply.github.com>
2024-02-11 04:45:57 +0000

    First operation
    First operation try

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,15 @@
 def arithmetic_arranger(problems):
 
-
-    return arranged_problems
+  arranged_problems = ""
+  
+  if len(problems) > 5:
+    return "Error: Too many problems."
+  
+  a = problems[0].split(' + ')
+  length = max(len(a[0]), len(a[1]))+2
+  spaces_first_line = length - len(a[0])
+  spaces_second_line = length - len(a[1])-1
+  
+  print(' '*spaces_first_line+a[0]+'\n+'+' '*spaces_second_line+a[1]+'\n'+'-'*length)
+  
+  return arranged_problems
